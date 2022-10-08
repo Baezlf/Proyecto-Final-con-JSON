0840441fa358024fa7e36c8745c7dd46c545fe6a
Lucas Baez <baez.lucasf@gmail.com>
2022-10-07 23:11:46 -0300

    modificado

--- index.js
+++ index.js
@@ -17,6 +17,8 @@ class Manga {
 }
 
 let carrito = [];
+let mangasGuardados = [];
+//let verMangasVendidos = [];
 
 const contenedor = document.getElementById("contenedor");
 const carritoComprasHTML = document.getElementById("carrito");
@@ -30,39 +32,71 @@ const cargarMangasJSON = async () => {
     let response = await fetch("./mangas.json");
     let mangas = await response.json();
     localStorage.setItem("mangasGuardados", JSON.stringify(mangas));
+
+}
+//mostrar productos en la pagina
+
+const mostrarEnPagina =  () => {
+    mangasGuardados = JSON.parse(localStorage.getItem("mangasGuardados"));
+    mangasGuardados.forEach(producto => {
+        let {id, titulo, autor, marca, formato, precio, stock, img} = producto;
+        const div = document.createElement(`div`);  
+        let agregar = `<button class="boton" id="agregar-prod", onclick="agregarProducto(${id})"><div id="icono"><i class="bi bi-cart-plus"></i></div> <div>Agregar al carrito</div></button>
+        </div>
+        `;
+        let sinStock = `<p><b>Sin Stock</b></p>
+        </div>
+        `;
+        let divHTML = `
+            <div class="mostrar-productos">
+                <div class="div-imagen">
+                <img class="imagen-manga" src="${img}">
+                </div>
+                <h3 class="titulo-manga">"${titulo}"</h3>
+                <p>Autor: ${autor}<br>
+                Editorial: ${marca} <br>
+                Formato: ${formato}<br>
+                Precio: <b>$${precio}</b></p>`;
+        stock > 0 ? divHTML +=agregar : divHTML +=sinStock;
+        div.innerHTML +=divHTML;
+        contenedor.append(div);
+    });        
+};
+
+const recargarPagina = () => {
+    location.href = location.href;
 }
 
-cargarMangasJSON();
+if(!localStorage.getItem("mangasGuardados")){
+    cargarMangasJSON()
+    .then(() => {
+        return mostrarEnPagina();
+    })
+}else{
+    mostrarEnPagina();
+}
 
-//mostrar productos en la pagina
-let mangasGuardados = JSON.parse(localStorage.getItem("mangasGuardados"));
-mangasGuardados.forEach(producto => {
-    let {id, titulo, autor, marca, formato, precio, stock, img} = producto;
-    const div = document.createElement(`div`);  
-    let agregar = `<button class="boton" id="agregar-prod", onclick="agregarProducto(${id})"><div id="icono"><i class="bi bi-cart-plus"></i></div> <div>Agregar al carrito</div></button>
-    </div>
-    `;
-    let sinStock = `<p><b>Sin Stock</b></p>
-    </div>
-    `;
-    let divHTML = `
-        <div class="mostrar-productos">
-            <div class="div-imagen">
-            <img class="imagen-manga" src="${img}">
-            </div>
-            <h3 class="titulo-manga">"${titulo}"</h3>
-            <p>Autor: ${autor}<br>
-            Editorial: ${marca} <br>
-            Formato: ${formato}<br>
-            Precio: <b>$${precio}</b></p>`;
-    stock > 0 ? divHTML +=agregar : divHTML +=sinStock;
-    div.innerHTML +=divHTML;
-    contenedor.append(div);
-});
+
 
 //Carrito de compras
 let guardarCarrito = () => {
     localStorage.setItem("carritoStorage", JSON.stringify(carrito));
+}
+
+let renderizarCarrito = () =>{
+    let carritoHTML = "";
+    carrito.forEach((prod, id) => {
+        let {titulo, precio, cantidad} = prod;
+        carritoHTML += `
+        <div class="prod-en-carrito">
+            <h3>"${titulo}"</h3>
+            Cantidad: <b>${cantidad}</b><br>
+            <p>Precio: <span id="total">$${precio}</span></p><br>
+            <button class="boton" onclick="eliminarProdCarrito(${id})">Eliminar<i class="bi bi-cart-dash"></i></button><br><br>
+        </div>            
+        `
+    });
+    carritoComprasHTML.innerHTML = carritoHTML;
 }
 
 let eliminarProdCarrito = (id) => {
@@ -105,22 +139,6 @@ let vaciarCarritoConfirmar = () => {
 }
 
 vaciar.addEventListener("click", vaciarCarritoConfirmar);
-
-let renderizarCarrito = () =>{
-    let carritoHTML = "";
-    carrito.forEach((prod, id) => {
-        let {titulo, precio, cantidad} = prod;
-        carritoHTML += `
-        <div class="prod-en-carrito">
-            <h3>"${titulo}"</h3>
-            Cantidad: <b>${cantidad}</b><br>
-            <p>Precio: <span id="total">$${precio}</span></p><br>
-            <button class="boton" onclick="eliminarProdCarrito(${id})">Eliminar<i class="bi bi-cart-dash"></i></button><br><br>
-        </div>            
-        `
-    });
-    carritoComprasHTML.innerHTML = carritoHTML;
-}
 
 let calcularTotal = () => {
     let total = 0;
@@ -195,14 +213,13 @@ let realizarCompra = () => {
     }
     
     for(const prod of carrito){
-        console.log(prod);
         let pos = prod.id - 1;
-        console.log(mangasGuardados[pos])
         mangasGuardados[pos].stock -= prod.cantidad;
         mangasGuardados[pos].cantidadVendida ? mangasGuardados[pos].cantidadVendida += prod.cantidad : mangasGuardados[pos].cantidadVendida = prod.cantidad;
     }
     guardarMangasLS();
     vaciarCarrito();
+    recargarPagina();
 };
 
 comprar.addEventListener("click", realizarCompra)
